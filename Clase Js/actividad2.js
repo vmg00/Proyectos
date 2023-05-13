99d9e464ddf5034a63d5be475190ec7475094b89
Valentina <valenmena4@gmail.com>
2023-05-13 01:44:52 +0000

    segundo ejercicio de javascript

---
+++ Clase Js/actividad2.js
@@ -0,0 +1,33 @@
+function mayorArray(array) {
+    if (array.lenght == 0) {
+        return null;
+    }
+
+    else
+        (array.lenght == 1); {
+        return array[0];
+    }
+
+    let numMayor = array[0];
+    for (let i = 1; i < array.length; i++) {
+    }
+
+    let i = 0;
+    while (i < array.length) {
+        console.log(arr[i]);
+        i++;
+    }
+    let numImpares = 0;
+    let b = 0;
+
+    do {
+        if (array[b]) {
+            numImpares++;
+        }
+        i++;
+    } while (b < array.length);
+
+    console.log("La cantidad de números impares es:", numImpares);
+    return numMayor;
+
+}
